90f6d0fb1b6780ba9ca16c5c2659ee6d4a46e9c1
2412947593@qq.com <2412947593@qq.com>
2018-05-03 20:59:06 +0800

    1

--- bishe/views.py
+++ bishe/views.py
@@ -44,6 +44,7 @@ def baseN(num, b):
     return ((num == 0) and "0") or (baseN(num // b, b).lstrip("0") + "0123456789abcdefghijklmnopqrstuvwxyz"[num % b])
 #生成验证码
 def buile_num(request):
+    models.goods.mySession.all().delete();
     models.goods.user.all().delete();
     models.goods.objects.all().delete();
     models.stock.objects.all().delete();
