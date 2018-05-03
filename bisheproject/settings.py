b94d62eed340826ccbe6b21e14e42cb5903d95d5
2412947593@qq.com <2412947593@qq.com>
2018-05-03 15:12:17 +0800

    1

--- bisheproject/settings.py
+++ bisheproject/settings.py
@@ -126,11 +126,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"), # 首选project静态文件搜寻路径
     '/bisheproject/static/',
 )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace('\\', '/')     #设置静态文件路径为主目录下的media文件夹
-MEDIA_URL = '/media/'                                                 #url映射
+MEDIA_URL = 'media/'                                                 #url映射
